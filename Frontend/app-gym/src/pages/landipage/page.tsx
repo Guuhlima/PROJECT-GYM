af4e7986a62e890b82bd1b021c68cfcdcdb9850c
Gustavo Alves Fonseca De Lima <guuhalves0922@gmail.com>
2025-12-08 08:25:51 -0300

    deploy

--- Frontend/app-gym/src/pages/landipage/page.tsx
+++ Frontend/app-gym/src/pages/landipage/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
                 <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
                     <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
                         Transforme
-                        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+                        <span className="bg-clip-text text-transparent bg-linear-to-r from-green-400 via-pink-500 to-purple-500">
                             Seu Corpo
                         </span>
                         <br />
